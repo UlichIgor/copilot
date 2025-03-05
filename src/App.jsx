70d9e33053f6c4d24054fc1696b4600811121a66
Ulich Igor <ulichigor55@gmail.com>
2025-03-05 09:14:43 +0200

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './scss/styles.module.scss';
 import Header from './components/Header/Header.jsx';
+import Content from './components/Content/Content.jsx';
+import Footer from './components/Footer/Footer.jsx';
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
+      <Content />
+      <Footer/>
     </>
   );
 }
