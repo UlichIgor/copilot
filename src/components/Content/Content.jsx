98742b411e2417eb638bc4d0bf468b41e99c17dd
Ulich Igor <ulichigor55@gmail.com>
2025-03-05 15:53:08 +0200

    .Content

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import styles from './Content.module.scss';
+import Images from '../../images/poster.png';
 export default class Content extends Component {
   render() {
     return (
-      <div className={styles.content}><h2>Content</h2></div>
+      <div className={styles.content}><h2>Content</h2>
+        <img className={styles.images} src={Images} alt="poster" />
+      </div>
     )
   }
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,10 @@ import styles from './Footer.module.scss';
 export default class Footer extends Component {
   render() {
     return (
-      <div className={styles.footer}><h3>Footer</h3></div>
+      <div className={styles.footer}><h3>Footer</h3>
+    
+      </div>
+      
     )
   }
 }
